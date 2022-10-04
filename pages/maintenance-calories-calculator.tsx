4ab52b450e049b25c612fe3a8a74195a152e63bd
Cristian Rosca <cristianrrosca@gmail.com>
2022-10-04 14:10:48 +0100

    initial commit

---
+++ pages/maintenance-calories-calculator.tsx
@@ -0,0 +1,17 @@
+const MaintenanceCaloriesCalculator = () => {
+    return ( 
+        <div>
+            <h1>Nutrition Calculator</h1>
+            <h2>Calculate your maintenance calorie intake</h2>
+            <form>
+                <label>Inputs</label>
+                <input></input>
+                <input></input>
+                <input></input>
+                <label>Outputs</label>
+            </form>
+        </div>
+     );
+}
+ 
+export default MaintenanceCaloriesCalculator;
