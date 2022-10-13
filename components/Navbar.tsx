4c42eae05d59c72e2fe1065d7efdcf0982adf9f2
Cristian Rosca <cristianrrosca@gmail.com>
2022-10-13 15:42:28 +0100

    add interface and RateOfWeightChange object + legend

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
                 </Flex>
                 <Flex flexDirection={"row"} justifyContent={"space-around"}>
                     <Link href="/maintenance-calories-calculator"><a>Step 1: Calculate Maintenance Calories</a></Link>
-                    <Link href="/"><a>Step 2: Set Rate of Progression</a></Link>
+                    <Link href="/daily-calorie-calculator"><a>Step 2: Set Rate of Progression</a></Link>
                     <Link href="/"><a>Step 3: Calculate Intake Recommendation</a></Link>
                     <Link href="/"><a>Step 4: Calculate Refeed Intake</a></Link>
                     <Link href="/"><a>Troubleshooting Nutrition Targets</a></Link>
--- pages/daily-calorie-calculator.tsx
+++ pages/daily-calorie-calculator.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { Divider, Flex, Heading, Input, Select, Text, } from "@chakra-ui/react";
+import Head from "next/head";
 
 const DailyCalorieCalculator = () => {
+
+    interface RateOfWeightChange {
+        label : string,
+        value : number
+    }
 
     const [userBodyWeight, setUserBodyWeight] = React.useState<number>(0)
     const handleUserBodyWeightChange = (e : React.ChangeEvent<HTMLInputElement>) => setUserBodyWeight(Number(e.target.value))
@@ -9,58 +16,68 @@ const DailyCalorieCalculator = () => {
     const handleUserSexChange = (e : React.ChangeEvent<HTMLSelectElement>) => setUserSex(e.target.value)
     
     const [userBodyFatPercentage, setUserBodyFatPercentage] = React.useState<number>(0)
-    const handleUserBodyFatPercentageChange = (e : React.ChangeEvent<HTMLSelectElement>) => setUserBodyFatPercentage(Number(e.target.value))
+    const handleUserBodyFatPercentageChange = (e : React.ChangeEvent<HTMLInputElement>) => setUserBodyFatPercentage(Number(e.target.value))
     
     const [userMaintenanceCalories, setUserMaintenanceCalories] = React.useState<number>(0)
+    const handleUserMaintenanceCaloriesChange = (e : React.ChangeEvent<HTMLInputElement>) => setUserMaintenanceCalories(Number(e.target.value))
     
     const [userGoal, setUserGoal] = React.useState<string>('')
     const handleUserGoalChange = (e : React.ChangeEvent<HTMLSelectElement>) => setUserGoal(e.target.value)
     
-    const [availableUserRatesOfWeightChange, setAvailableUserRatesOfWeightChange] = React.useState<number[]>([])
+    const [availableUserRatesOfWeightChange, setAvailableUserRatesOfWeightChange] = React.useState<RateOfWeightChange[]>([])
     
     const [userRateOfWeightChange, setUserRatesOfWeightChange] = React.useState<number>(0)
-    const handleUserRatesOfWeightChangeChange = (e : React.ChangeEvent<HTMLInputElement>) => setUserRatesOfWeightChange(Number(e.target.value))
+    const handleUserRatesOfWeightChangeChange = (e : React.ChangeEvent<HTMLSelectElement>) => setUserRatesOfWeightChange(Number(e.target.value))
 
     const [userDailyCalorieTarget, setUserDailyCalorieTarget] = React.useState<number>(0)
 
 
+    const weightChangeLegend = {
+        "slowGain" : {label : "Gain 0.5% of your body weight per month (Advanced Trainees)", value : 0.005},
+        "mediumGain" : {label : "Gain 1% of your body weight per month (Intermediate Trainees)", value : 0.01},
+        "fastGain" : {label : "Gain 1.5% of your body weight per month (Beginner Trainees)", value : 0.015},
+        "slowDiet" : {label : "Lose 2% of your body weight per month", value : -0.02},
+        "mediumDiet" : {label : "Lose 3% of your body weight per month", value : -0.03},
+        "fastDiet" : {label : "Lose 4% of your body weight per month", value : -0.04}
+    }
+
     function findAvailableUserRatesOfWeightChange(sex: string, goal : string, bodyFatPercentage: number) {
         if(sex === "Male"){
             if (goal === "gaining"){
-                setAvailableUserRatesOfWeightChange([0.005, 0.01, 0.015])
+                setAvailableUserRatesOfWeightChange([weightChangeLegend["slowGain"], weightChangeLegend["mediumGain"], weightChangeLegend["fastGain"]])
             }
             else if (goal === "dieting"){
                 if (bodyFatPercentage <= 10){
-                    setAvailableUserRatesOfWeightChange([-0.02])
+                    setAvailableUserRatesOfWeightChange([weightChangeLegend["slowDiet"]])
                 }
                 else if (bodyFatPercentage <= 15){
-                    setAvailableUserRatesOfWeightChange([-0.02, -0.03])
+                    setAvailableUserRatesOfWeightChange([weightChangeLegend["slowDiet"], weightChangeLegend["mediumDiet"]])
                 }
                 else if (bodyFatPercentage >= 16){
-                    setAvailableUserRatesOfWeightChange([-0.02, -0.03, -0.04])
+                    setAvailableUserRatesOfWeightChange([weightChangeLegend["slowDiet"], weightChangeLegend["mediumDiet"], weightChangeLegend["fastDiet"]])
                 }
             }
         }
         else if (sex === "Female"){
             if (goal === "gaining"){
-                setAvailableUserRatesOfWeightChange([0.005, 0.01, 0.015])
+                setAvailableUserRatesOfWeightChange([weightChangeLegend["slowGain"], weightChangeLegend["mediumGain"], weightChangeLegend["fastGain"]])
             }
             else if (goal === "dieting"){
                 if (bodyFatPercentage <= 20){
-                    setAvailableUserRatesOfWeightChange([-0.02])
+                    setAvailableUserRatesOfWeightChange([weightChangeLegend["slowDiet"]])
                 }
                 else if (bodyFatPercentage <= 27){
-                    setAvailableUserRatesOfWeightChange([-0.02, -0.03])
+                    setAvailableUserRatesOfWeightChange([weightChangeLegend["slowDiet"], weightChangeLegend["mediumDiet"]])
                 }
                 else if (bodyFatPercentage >= 28){
-                    setAvailableUserRatesOfWeightChange([-0.02, -0.03, -0.04])
+                    setAvailableUserRatesOfWeightChange([weightChangeLegend["slowGain"], weightChangeLegend["mediumGain"], weightChangeLegend["fastGain"]])
                 }
             }
         }
 
     }
     
-    findAvailableUserRatesOfWeightChange(userSex, userGoal, userBodyFatPercentage)
+    // findAvailableUserRatesOfWeightChange(userSex, userGoal, userBodyFatPercentage)
 
     function calculateDailyCalorieTarget(bodyWeightInKG: number, maintenanceCalorieIntake: number, rateOfWeightChange: number){
         const kcalsPerKG : number = 7700
@@ -72,13 +89,52 @@ const DailyCalorieCalculator = () => {
         setUserDailyCalorieTarget(dailyCalorieTarget)
     }
 
-    calculateDailyCalorieTarget(userBodyWeight, userMaintenanceCalories, userRateOfWeightChange)
+    // calculateDailyCalorieTarget(userBodyWeight, userMaintenanceCalories, userRateOfWeightChange)
 
 
 
     return ( 
         <>
+        <Head>
+            <title>Nutrition Tool | Maintenance Calorie Calculator</title>
+            <meta />
+        </Head>
+        <Flex height={"50rem"} alignItems={"center"} justifyContent={"center"}>
+        <Flex direction={"column"} p={10} rounded={6} position={"relative"} background={"blackAlpha.100"} alignItems={"center"}>
+            <Heading mb={"5"}>Calculate Your Daily Calorie Target</Heading>
+            <Flex direction={"column"} width="50%">
+            <Text alignSelf={"start"} mb='8px'>Body Weight in KG:</Text>
+            <Input  mb={3} variant={"outlined"} value={userBodyWeight} onChange={handleUserBodyWeightChange} ></Input>
+            <Text mb='8px'>Sex:</Text>
+            <Select bg='white' placeholder="Select from dropdown"  mb={3} onChange={handleUserSexChange}>
+                <option value='Male'>Male</option>
+                <option value='Female'>Female</option>
+            <Text mb='8px'>Body Fat Percentage:</Text>
+            <Input mb={3}  variant={"outlined"} value={userBodyFatPercentage} onChange={handleUserBodyFatPercentageChange}></Input>
+            <Text mb='8px'>Maintenance Calories:</Text>
+            <Input mb={3} variant={"outlined"} value={userMaintenanceCalories} onChange={handleUserMaintenanceCaloriesChange}></Input>
+            </Select>
+            <Text mb='8px'>Your Goal:</Text>
+            <Select bg='white' placeholder="Select from dropdown"  mb={3} onChange={handleUserSexChange}>
+                <option value='Gaining'>Gain Weight</option>
+                <option value='Dieting'>Lose Weight</option>
+            </Select>
+            <Text mb='8px'>Target Rate of Weight Change:</Text>
+            <Select bg='white' placeholder="Select from dropdown" mb={3} onChange={handleUserRatesOfWeightChangeChange} whiteSpace="normal">
+                <option value='1'>yet to implement</option>
+                <option value='1'>yet to implement</option>
+                <option value='1'>yet to implement</option>
+            </Select>
+            </Flex>
+            <Divider mb={"3"} mt={"3"} orientation='horizontal' />
+            <Heading mb={"3"} size='lg'> Results</Heading>
+            <Text fontSize='lg'> Your Daily Calorie Target is: {userDailyCalorieTarget}kcal</Text>
+            {/* <Text fontSize='lg'> Your target monthly weight change is: {} kcal</Text> */}
+
+
+        </Flex>
         
+        </Flex>
         </>
      );
 }
