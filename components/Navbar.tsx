1caf0337b183e8a962345c5183df6ff7ba3fb81b
Cristian Rosca <cristianrrosca@gmail.com>
2022-10-29 18:12:13 +0100

    add placeholder hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,62 @@
 import Link from "next/link";
 import Image from "next/image";
-import {Box} from "@chakra-ui/react"
+import { Box, IconButton } from "@chakra-ui/react"
 import brandLogo from "../public/imperium.png"
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    MenuItemOption,
+    MenuGroup,
+    MenuOptionGroup,
+    MenuDivider,
+} from '@chakra-ui/react'
+import { HamburgerIcon, AddIcon, ExternalLinkIcon, RepeatIcon, EditIcon } from '@chakra-ui/icons'
 
 
 const Navbar = () => {
-    return ( 
+    return (
         <nav>
-            <Box display={"flex"}flexDirection={"column"} justifyContent={"space-between"} >
-                <Box display={"flex"} mt={5} mb={5}justifyContent={"center"}>
-                <Link href="/">
-                    <a>
-                        <Image src={brandLogo} height={"90px"} width={"90px"}/>
-                    </a>
-                </Link>
+            <Box display={"flex"} justifyContent={"space-between"}>
+                <Box display={"flex"} my={"2rem"} ml={"2rem"} >
+                    <Link href="/">
+                        <a>
+                            <Image src={brandLogo} height={"90px"} width={"90px"} />
+                        </a>
+                    </Link>
                 </Box>
-                <Box display={"flex"} color={"white"} as='b'flexDirection={"row"} justifyContent={"space-around"}>
-                    <Link href="/maintenance-calories-calculator"><a>Step 1: Calculate Maintenance Calories</a></Link>
-                    <Link href="/daily-calorie-calculator"><a>Step 2: Calculate Daily Calories</a></Link>
-                    <Link href="/macro-calculator"><a>Step 3: Calculate Daily Macros</a></Link>
-                    <Link href="/"><a>Step 4: Calculate Refeed Intake</a></Link>
-                    <Link href="/"><a>Troubleshooting Nutrition Targets</a></Link>
+                <Box display={"flex"} my={"2rem"} mr={"2rem"} >
+                    <Menu>
+                        <MenuButton
+                            color={"white"}
+                            boxSize={"4rem"}
+                            as={IconButton}
+                            aria-label='Options'
+                            icon={<HamburgerIcon />}
+                            variant='outline'
+                        />
+                        <MenuList>
+                            <MenuItem icon={<AddIcon />} command='⌘T'>
+                                New Tab
+                            </MenuItem>
+                            <MenuItem icon={<ExternalLinkIcon />} command='⌘N'>
+                                New Window
+                            </MenuItem>
+                            <MenuItem icon={<RepeatIcon />} command='⌘⇧N'>
+                                Open Closed Tab
+                            </MenuItem>
+                            <MenuItem icon={<EditIcon />} command='⌘O'>
+                                Open File...
+                            </MenuItem>
+                        </MenuList>
+                    </Menu>
                 </Box>
+
             </Box>
         </nav>
 
     );
 }
- 
+
 export default Navbar;
